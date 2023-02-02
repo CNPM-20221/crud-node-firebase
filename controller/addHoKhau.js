283ce14ec9a19b1acc4a418d5e2f26da0ada0894
hung1012 <57567619+hung1012@users.noreply.github.com>
2023-02-02 23:07:16 +0700

    fix: hokhau

--- controller/addHoKhau.js
+++ controller/addHoKhau.js
@@ -4,19 +4,19 @@ module.exports.addHoKhau = async (req, res) => {
   const {idChuho,sothanhvien,diachi} = req.body;
   try {
     const entry = db.collection('hokhau').doc();
-    const peopleObject = {
+    const ChuhoObject = {
       maho :entry.id,
       sothanhvien,
       diachi,
       idChuho
 
-    };
+    }
       
-      entry.set(peopleObject);
+      entry.set(ChuhoObject);
         res.status(200).send({
           status: 'success',
-          message: 'HoKhau added, register successfully !',
-          data: peopleObject,
+          message: 'HoKhau added successfully !',
+          data: ChuhoObject,
         });
   } catch (error) {
     res.status(500).json(error.message);
--- controller/addNhanKhau.js
+++ controller/addNhanKhau.js
@@ -16,15 +16,31 @@ module.exports.addNhanKhau = async (req, res) => {
       identify,
       address,
       status
-
-    };
-      
+    }
+    if (chuHo==true) {
+      const dbquery = db.collection('hokhau').doc()
+      const ChuhoObject = {
+        maho :dbquery.id,
+        sothanhvien:"",
+        diachi:"",
+        idChuho: entry.id
+      }
+      dbquery.set(ChuhoObject);
+        res.status(200).send({
+          status: 'success',
+          message: 'HoKhau added successfully !',
+          data: ChuhoObject,
+        });
+    }
+    else {
       entry.set(peopleObject);
         res.status(200).send({
           status: 'success',
           message: 'Nhan khau added, register successfully !',
           data: peopleObject,
         });
+    }
+      
   } catch (error) {
     res.status(500).json(error.message);
   }
