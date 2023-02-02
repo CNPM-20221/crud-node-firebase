69797bef45765743d980c3c0d6656041df08ce63
hung1012 <57567619+hung1012@users.noreply.github.com>
2023-02-02 23:42:19 +0700

    fix: getChiphi

--- controller/getChiPhi.js
+++ controller/getChiPhi.js
@@ -7,21 +7,21 @@ module.exports.chiphi_pagination = async (req, res, next) => {
       let size = parseInt(req.query.size)
       const list_chiphi = []
       if (n_cursor) {
-        const querydb = await db.collection('chiphi').orderBy('fullname')
+        const querydb = await db.collection('chiphi').orderBy('id')
                                 .startAt(n_cursor)
                                 .limit(size+1)
                                 .get()
         querydb.forEach((doc) => list_chiphi.push(doc.data()))
       }
       else {
-        const querydb = await db.collection('chiphi').orderBy('fullname')
+        const querydb = await db.collection('chiphi').orderBy('id')
                                 .limit(size+1)
                                 .get()
         querydb.forEach((doc) => list_chiphi.push(doc.data()))
       }
       return res.status(200).json({
         data: list_chiphi.slice(0,size),
-        nextCursor: list_chiphi[size].fullname,
+        nextCursor: list_chiphi[size].id,
         size: size   
       })
       } 
